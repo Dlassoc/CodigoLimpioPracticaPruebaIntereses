b4f714f5e5146a282fdb5f0545892460d39870d0
Daniel Lasso <dlassoc123@gmail.com>
2023-08-09 15:57:29 -0500

    #R2 COMLETO, FALTA EL R3

--- tests.py
+++ tests.py
@@ -86,7 +86,7 @@ class MyTestCase(unittest.TestCase):
         interes = 0.031
         extra = 53000
         plan = Ejercicio.plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, extra)
-        self.assertEqual(len(plan), cuotas)
+        self.assertEqual(len(plan), cuotas)#a
 
 if __name__ == '__main__':
     unittest.main()
