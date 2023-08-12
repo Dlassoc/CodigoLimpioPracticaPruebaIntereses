aa34e78df6048253ac2c118c1b6d134ff957ef57
Daniel Lasso <dlassoc123@gmail.com>
2023-08-12 15:35:40 -0500

    TERMINADOOOO

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,16 +1,23 @@
-def calcular_cuota_mensual(valor_prodcto, interes, cuotas):
+from tabulate import tabulate  # Tiene que instalar para correr el programa la libreria tabulate
+
+
+class Except_Valor(Exception):
+    pass
+
+
+def calcular_cuota_mensual(valor_producto, interes, cuotas):
     if interes == 0 or cuotas == 1:
         return 0
     elif interes >= 0.12:
-        return "NO ES POSIBLE"
-    elif valor_prodcto == 0:
-        return "El valor del producto debe ser mayor a 0"
+        raise Except_Valor("El valor supera los intereses óptimos")
+    elif valor_producto == 0:
+        raise Except_Valor("El valor del producto debe ser mayor a 0")
     elif cuotas < 0:
-        return "Las cuotas no pueden ser negativas"
+        raise Except_Valor("Las cuotas no pueden ser negativas")
     else:
-
-        cuota_mes = (valor_prodcto * interes) / (1 - (1 + interes) ** (-cuotas))
-        return calcular_intereses_antes_de_pagar(cuota_mes, valor_prodcto, cuotas)
+        # Calcular la cuota mensual
+        cuota_mes = (valor_producto * interes) / (1 - (1 + interes) ** (-cuotas))
+        return calcular_intereses_antes_de_pagar(cuota_mes, valor_producto, cuotas)
 
 
 def calcular_intereses_antes_de_pagar(cuota_mes, valor_prodcto, cuotas):
@@ -23,6 +30,8 @@ def plan_amortizacion(cuota_mes, valor_producto, cuotas, interes):
     saldo_restante = valor_producto
 
     for mes in range(1, cuotas + 1):
+        if interes * 100 > 12:
+            raise Except_Valor("EL VALOR ES MUY ALTO, AVARO")
         pago_interes = saldo_restante * interes
         abono_capital = cuota_mes - pago_interes
         saldo_restante -= abono_capital
@@ -48,26 +57,29 @@ def plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, abo
         mes += 1
         cuota_real = cuota_mes
 
+        pago_interes = saldo_restante * interes
+        abono_capital = cuota_real - pago_interes
+
+        if abono_extra > cuota_real:
+            raise Except_Valor("Error: el abono extra supera el saldo restante en el mes especificado")
+
         if mes == cuota_mes_extra:  # Aplicar el abono extra en el mes especificado
-            pago_interes = saldo_restante * interes
+            cuota_real = abono_extra
             abono_capital = abono_extra - pago_interes
-        else:
-            pago_interes = saldo_restante * interes
-            abono_capital = cuota_real - pago_interes
 
         if abono_capital > saldo_restante:
             abono_capital = saldo_restante
 
         saldo_restante -= abono_capital
 
-        cuota_info = {
-            "Mes": mes,
-            "Saldo Inicial": saldo_restante + abono_capital,
-            "Pago Mensual": cuota_real,
-            "Intereses": pago_interes,
-            "Capital": abono_capital,
-            "Saldo Restante": saldo_restante
-        }
+        cuota_info = [mes,
+                      round(saldo_restante + abono_capital),
+                      round(cuota_real, 3),
+                      round(pago_interes, 3),
+                      round(abono_capital, 3),
+                      round(saldo_restante, 3)]
         tabla_amortizacion.append(cuota_info)
-    print(tabla_amortizacion)
+
+    headers = ["Mes", "Saldo Inicial", "Pago Mensual", "Intereses", "Capital", "Saldo Restante"]
+    print(tabulate(tabla_amortizacion, headers=headers, tablefmt="simple"))
     return tabla_amortizacion
--- tests.py
+++ tests.py
@@ -35,48 +35,51 @@ class MyTestCase(unittest.TestCase):
                                places=2)  # places indica la cantidad de decimales a comparar
 
     def testPayment4(self):
-        valor_prodcto = 480000
+        valor_producto = 480000
         interes = 0.125
         cuotas = 48
-        valor_total_intereses = "NO ES POSIBLE"
-        resultado = Ejercicio.calcular_cuota_mensual(valor_prodcto, interes, cuotas)
-
-        # Verificar si el resultado es igual a "NO ES POSIBLE" o 0
-        self.assertEqual(valor_total_intereses, resultado)
+        try:
+            resultado = Ejercicio.calcular_cuota_mensual(valor_producto, interes, cuotas)
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "El valor supera los intereses óptimos")
 
     def testPayment5(self):
-        valor_prodcto = 0
+        valor_producto = 0
         interes = 0.024
         cuotas = 60
-        valor_total_intereses = "El valor del producto debe ser mayor a 0"
-        resultado = Ejercicio.calcular_cuota_mensual(valor_prodcto, interes, cuotas)
-
-        self.assertEqual(valor_total_intereses, resultado)
+        try:
+            resultado = Ejercicio.calcular_cuota_mensual(valor_producto, interes, cuotas)
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "El valor del producto debe ser mayor a 0")
 
     def testPayment6(self):
-        valor_prodcto = 50000
+        valor_producto = 50000
         interes = 0.01
         cuotas = -10
-        valor_total_intereses = "Las cuotas no pueden ser negativas"
-        resultado = Ejercicio.calcular_cuota_mensual(valor_prodcto, interes, cuotas)
-
-        self.assertEqual(valor_total_intereses, resultado)
+        try:
+            resultado = Ejercicio.calcular_cuota_mensual(valor_producto, interes, cuotas)
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "Las cuotas no pueden ser negativas")
 
     def test_plan_amortizacion(self):
         cuota_mes = 9297.96
         valor_producto = 200000
         cuotas = 36
         interes = 0.031
-        plan = Ejercicio.plan_amortizacion(cuota_mes, valor_producto, cuotas, interes)
-        self.assertEqual(len(plan), cuotas)
+        try:
+            plan = Ejercicio.plan_amortizacion(cuota_mes, valor_producto, cuotas, interes)
+        except Ejercicio.Except_Valor as e:
+           self.assertEqual(str(e), "Las cuotas no pueden ser negativas")
 
     def test_plan_amortizacion_2(self):
         cuota_mes = 10000
         valor_producto = 480000
         cuotas = 48
         interes = 0
-        plan = Ejercicio.plan_amortizacion(cuota_mes, valor_producto, cuotas, interes)
-        self.assertEqual(len(plan), cuotas)
+        try:
+            plan = Ejercicio.plan_amortizacion(cuota_mes, valor_producto, cuotas, interes)
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "Las cuotas no pueden ser negativas")
 
     def test_plan_amortizacion_extra_1(self):
         cuota_mes = 9297.96
@@ -94,8 +97,37 @@ class MyTestCase(unittest.TestCase):
         cuotas = 23
         interes = 0.034
         extra = 90000
-        plan = Ejercicio.plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, extra, 10)
+        mes_abono_extra = 5
+        plan = Ejercicio.plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, extra, mes_abono_extra)
         self.assertEqual(len(plan), cuotas)
+
+
+    def test_plan_amortizacion_extra_3(self):
+        cuota_mes = 52377
+        valor_producto = 850000
+        cuotas = 23
+        interes = 0.034
+        extra = 90000
+        mes_abono_extra = 10
+        try:
+            plan = Ejercicio.plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, extra, mes_abono_extra)
+
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "El abono debe ser mayor a la cuota base ")
+
+    def test_plan_amortizacion_extra_3(self):
+        cuota_mes = 52377
+        valor_producto = 850000
+        cuotas = 23
+        interes = 0.034
+        extra = 90000
+        mes_abono_extra = 22
+        try:
+            plan = Ejercicio.plan_amortizacion_pago_extra(cuota_mes, valor_producto, cuotas, interes, extra, mes_abono_extra)
+
+        except Ejercicio.Except_Valor as e:
+            self.assertEqual(str(e), "El abono debe ser mayor a la cuota base ")
+
 
 if __name__ == '__main__':
     unittest.main()
